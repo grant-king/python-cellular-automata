27d50d0b943ec906f3681f4ac90b72eadab61c56
Grant King <grantryan.king@gmail.com>
2019-11-21 23:21:55 -0800

    Create example_1.py
    Copy from early development repository at grant-king/simulations/simulations/configurable_grid_automatons/cga.py

---
+++ pythoncellularautomata/example_1.py
@@ -0,0 +1,36 @@
+import pygame
+from pygame.locals import QUIT
+from models.ca_models import Grid, Capture, Control
+
+SCREEN_SIZE = [1600, 900]
+CELL_SIZE = 10
+BACKGROUND_COLOR = [0, 0, 0]
+
+def main_loop():
+    pygame.init()
+
+    clock = pygame.time.Clock()
+    main_window = pygame.display.set_mode(SCREEN_SIZE)
+    
+    grid = Grid(CELL_SIZE, 'stains', aging=1)
+    capture = Capture(grid)
+    control = Control(capture)
+    
+    #grid.random_seed()
+    capture.load_image("D:/chaos/extra/cat_people.jpg")
+    control.step_clock.set_timer(QUIT, 300) #auto off after n steps
+    control.step_clock.set_timer(control.STATESHOTEVENT, 1) #capture state every n steps
+    
+    capture.state_shot()
+    while control.running:
+        grid.update()
+        capture.screen_shot()
+        pygame.display.set_caption(f'{grid.rule_set.name} step {grid.rule_set.run_ticks}')
+        pygame.display.flip()
+        capture.save_image()
+    #capture last state before quit        
+    capture.state_shot()
+    pygame.quit()
+
+if __name__ == '__main__':
+    main_loop()
