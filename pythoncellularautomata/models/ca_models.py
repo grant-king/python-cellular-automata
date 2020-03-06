ecfd0c64d63eb1f8bffa63efab36f4d87511dfcd
Grant King <grantryan.king@gmail.com>
2020-03-05 18:40:26 -0800

    Update ca_models.py
    Fix color mismatch when using update_grid_II(). Add switch_channels Grid method, to be called once from load_image() method in Capture class.
    
    Move age_colors to be ShotTool method in image_models.py file.
    
    Move Ruleset to new ruleset_models.py file.

--- pythoncellularautomata/models/ca_models.py
+++ pythoncellularautomata/models/ca_models.py
@@ -8,6 +8,7 @@ from time import time, ctime
 from models.performance_monitor import PerformanceMonitor
 from models.ca_models2 import Control, StepClock, StepTimer, Capture
 from models.image_models import ShotTool
+from models.ruleset_models import Ruleset
 
 class Grid:
     def __init__(self, cell_size, rule_name, aging=False):
@@ -25,11 +26,18 @@ class Grid:
         self.next_states = np.zeros((self.num_rows, self.num_columns), dtype=np.bool)
         self.color_channels = np.zeros((self.num_rows, self.num_columns, 3), dtype=np.float32) #store current color info
         self.rule_set = Ruleset(rule_name)
-        self.st = ShotTool()
+        self.st = ShotTool(self.rule_set)
         self.build_cells()
 
         logging.info(f'Grid initialized with {self.rule_set.name} rule at {ctime()} with {self.total_cells} cells')
         print(f'Grid initialized with {self.rule_set.name} rule at {ctime()} with {self.total_cells} cells')
+
+    def switch_channels(self):
+        """Switch red and blue color channels for proper coloring with update_grid_II 
+        """
+        intensity_channels = np.moveaxis(self.color_channels, -1, 0) #move axis for easy reordering
+        reordered_intensity_channels = [intensity_channels[2], intensity_channels[1], intensity_channels[0]]
+        self.color_channels = np.moveaxis(np.array((reordered_intensity_channels), dtype=np.int8), 0, -1) #return to standard image index format; rows, columns, channels
 
     def build_cells(self):
         self.cells = [[0 for row in range(self.num_rows)] for column in range(self.num_columns)]
@@ -63,7 +71,8 @@ class Grid:
         """update grid with Shot array methods
         """
         self.next_states = self.st.calculate_next_sequential(self.current_states)
-        self.age_colors()
+        if self.aging:
+            self.st.age_colors(self.color_channels, self.current_states)
         self.update_window()
 
     def update_window(self):
@@ -74,18 +83,10 @@ class Grid:
     def get_state_surface(self):
         #create image surface from current states, update to main window
         colored_states = np.moveaxis(self.color_channels, -1, 0) * self.current_states #make compatible for broadcast
-        colored_states = [cv2.resize(channel, self.image_size, interpolation=cv2.INTER_NEAREST) for channel in colored_states] #resize each channel
-        state_image = np.swapaxes(np.array((colored_states), dtype=np.int8), 0, -1) #return to standard image index format; rows, columns, channels
+        colored_states = [cv2.resize(channel, self.image_size, interpolation=cv2.INTER_NEAREST).T for channel in colored_states] #resize, rotate each channel
+        state_image = np.moveaxis(np.array((colored_states), dtype=np.int8), 0, -1) #return to standard image index format; rows, columns, channels
         state_image = pygame.pixelcopy.make_surface(state_image)
         return state_image
-    
-    def age_colors(self):
-        """get new colors by running each channel through age_channel_[...] ShotTool method"""
-        new_channels = []
-        for channel in np.moveaxis(self.color_channels, -1, 0):
-            new_channels.append(self.st.age_channel_sequential(channel, self.current_states))
-        new_channels = np.array(new_channels)
-        self.color_channels = np.moveaxis(new_channels, 0, -1)
 
     def refresh_cells(self, state_image):
         """
@@ -273,68 +274,4 @@ class CellLogic:
     def update(self, neighborhood_array): 
         self.neighborhood_array = neighborhood_array
         self.neighborhood_sum = neighborhood_array.sum()
-
-
-class Ruleset:
-    def __init__(self, name):
-        RULE_SETS = {
-            '2x2': {'survive': [1, 2, 5], 'born': [3, 6]}, 
-            'amoeba': {'survive': [1, 3, 5, 8], 'born': [3, 5, 7]},
-            'assimilation': {'survive': [4, 5, 6, 7], 'born': [3, 4, 5]},
-            'coagulations': {'survive': [2, 3, 5, 6, 7, 8], 'born': [3, 7, 8]},
-            'coral': {'survive': [4, 5, 6, 7, 8], 'born': [3]},
-            'conway': {'survive': [2, 3], 'born': [3]},
-            'daynight': {'survive': [3, 4, 6, 7, 8], 'born': [3, 6, 7, 8]},
-            'diamoeba': {'survive': [5, 6, 7, 8], 'born': [3, 5, 6, 7, 8]},
-            'electrifiedmaze': {'survive': [1, 2, 3, 4, 5], 'born': [4, 5]}, 
-            'flakes': {'survive': [0, 1, 2, 3, 4, 5, 6, 7, 8], 'born': [3]},
-            'flock': {'survive': [1, 2], 'born': [3]},
-            'fredkin': {'survive': [0, 2, 4, 6, 8], 'born': [1, 3, 5, 7]}, 
-            'gnarl': {'survive': [1], 'born': [1]}, 
-            'highlife': {'survive': [2, 3], 'born': [3, 6]}, 
-            'honeylife': {'survive': [2, 3, 8], 'born': [3, 8]}, 
-            'inverselife': {'survive': [3, 4, 6, 7, 8], 'born': [0, 1, 2, 3, 4, 7, 8]}, 
-            'ironflock': {'survive': [0, 2, 3], 'born': [3]},
-            'longlife': {'survive': [5], 'born': [3, 4, 5]},
-            'life34': {'survive': [3, 4], 'born': [3, 4]},
-            'lfod': {'survive': [0], 'born': [2]},
-            'maze': {'survive': [1, 2, 3, 4, 5], 'born': [3]}, 
-            'mazectric': {'survive': [1, 2, 3, 4], 'born': [3]}, 
-            'move': {'survive': [2, 4, 5], 'born': [3, 6, 8]},
-            'pedestrianlife': {'survive': [2, 3], 'born': [3, 8]},
-            'pseudolife': {'survive': [2, 3, 8], 'born': [3, 5, 7]}, 
-            'replicator': {'survive': [1, 3, 5, 7], 'born': [1, 3, 5, 7]}, 
-            'replicatorlog': {'survive': [2, 4, 5], 'born': [3, 6]}, 
-            'seeds': {'survive': [], 'born': [2]}, 
-            'serviettes': {'survive': [], 'born': [2, 3, 4]},
-            'stains': {'survive': [2, 3, 5, 6, 7, 8], 'born': [3, 6, 7, 8]},
-            'walledcities': {'survive': [2, 3, 4, 5], 'born': [4, 5, 6, 7, 8]},
-        }
-        self.init_time = time()
-        self.name = name
-        self.rule_set = RULE_SETS[self.name]
-        self.rule_survive = np.array(self.rule_set['survive'], np.int8)
-        self.rule_born = np.array(self.rule_set['born'], np.int8)
-        self.run_ticks = 0
-    
-    def apply_rules(self, cell):
-        """toggle cell state depending on current living state and rule set setting
-        """
-        if cell.cell_logic.alive:
-            if cell.neighborhood_sum not in self.rule_survive:
-                cell.toggle_cell(False) #kill cell
-        else:
-            if cell.neighborhood_sum in self.rule_born:
-                cell.toggle_cell(True) #revive cell
-
-    def add_tick(self):
-        self.run_ticks += 1
-
-    def __eq__(self, other):
-        if self.name == other.name:
-            return True
-        return False
-
-    def __str__(self):
-        return f'{self.name}'
 
