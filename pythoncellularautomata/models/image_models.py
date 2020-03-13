24d583e912580bbb5f964b0fe61af82d03d59d7e
Grant King <grantryan.king@gmail.com>
2020-03-12 23:26:14 -0700

    Update image_models.py
    Add ShotToolCUDA class method outlines.
    
    Add calculate_nex method for calculating next state shot from current state using new kernels.
    
    Add new CUDA kernels for efficiently operating on state shot and neighborhood sum arrays.

--- pythoncellularautomata/models/image_models.py
+++ pythoncellularautomata/models/image_models.py
@@ -1,5 +1,7 @@
 import numpy as np
 from models.ruleset_models import Ruleset
+from numba import cuda
+import math, time
 
 class ShotTool:
     def __init__(self, grid):
@@ -39,8 +41,7 @@ class ShotTool:
             if current_color_value > 20: 
                 return current_color_value - (current_color_value / 250) #control darkening rate
 
-
-    def calculate_next_sequential(self, state_shot):
+    def calculate_next(self, state_shot):
         """calculate next state frame, one cell at a time"""
         next_state_shot = np.zeros_like(state_shot)
         shape = state_shot.shape
@@ -59,16 +60,113 @@ class ShotTool:
 
 class ShotToolCUDA:
     """CUDA parallel processing methods for operating on state shots"""
-    def __init__(self):
+    def __init__(self, grid):
+        """methods for operating on state shots, represented as 
+        2d grids of binary or 8-bit values"""
+        self.rule_set = grid.rule_set
+        self.grid = grid
+    
+    def age_colors(self, color_channels, current_states, states_histories):
+        """get new colors by running each channel through age_channel_[...] ShotTool method"""
+        new_channels = []
+        #reshape to (3, rows, columns)
+        for channel in np.moveaxis(color_channels, -1, 0):
+            new_channels.append(self.age_channel(channel, current_states, states_histories))
+        new_channels = np.array(new_channels)
+        return np.moveaxis(new_channels, 0, -1)
+
+    def age_channel(self, input_channel, states, states_histories):
+        """increment or decrement each element in input for similar 
+        output based on states"""
+        color_vals = input_channel.flatten()
+        output = np.empty_like(color_vals)
+        flatter_history = states_histories.reshape(-1, states_histories.shape[-1])
+        for idx, state in enumerate(states.flatten()):
+            if state:
+                output[idx] = self.age_color(color_vals[idx], flatter_history[idx, :])
+            else:
+                output[idx] = color_vals[idx]
+        return np.reshape(output, input_channel.shape)
+        ### todo implement as CUDA
+        pass
+
+    def age_color(self, current_color_value, current_cell_history):
+        """return new color value within limits"""
+        if current_cell_history[-1]: # if last state was alive, age towards white
+            if current_color_value < 250:
+                return current_color_value + (current_cell_history.mean() / 2) #add according to average of last n states
+        else:# otherwise decrease color components
+            if current_color_value > 20: 
+                return current_color_value - (current_color_value / 250) #control darkening rate
+        #todo implement as CUDA
         pass
     
-    def age_channel(self, input_channel, states):
-        pass
-
-    def age_color(self, current_color_value):
-        pass 
-
     def calculate_next(self, state_shot):
-        """calculate next state frame in parallel with CUDA """
-        pass
+        """calculate next state frame, one cell at a time"""
+        """
+        next_state_shot = np.zeros_like(state_shot)
+        shape = state_shot.shape
+        for col_idx in range(1, shape[1] - 1):
+            for row_idx in range(1, shape[0] - 1): #clip edges
+                rows = (row_idx - 1, row_idx + 2)
+                cols = (col_idx - 1, col_idx + 2)
+                neighborhood = state_shot[rows[0]:rows[1], cols[0]:cols[1]].copy()
+                neighborhood[1, 1] = 0 #don't count center
+                state = state_shot[row_idx, col_idx]
+                new_state = self.rule_set.get_next_state(state, neighborhood.sum())
+                next_state_shot[row_idx, col_idx] = new_state
+        
+        return next_state_shot
+        """
+        #### todo implement above as CUDA, below
+        blockdim = (32, 32)
+        griddim = (state_shot.shape[0] // blockdim[0] + 1, state_shot.shape[1] // blockdim[1] + 1)
+
+        survive = self.rule_set.rule_survive
+        born = self.rule_set.rule_born
+
+        neighborhoods = np.zeros_like(state_shot, dtype=np.int8)
+        next_shot = np.zeros_like(state_shot)
+
+        #lauch kernels 
+        get_neighborhood_sums[griddim, blockdim](state_shot, neighborhoods)
+        get_next_shot[griddim, blockdim](state_shot, neighborhoods, next_shot, survive, born)
+
+        return next_shot
+
+
+@cuda.jit
+def get_neighborhood_sums(state_shot, neighborhoods_output):
+    threadx, thready = cuda.grid(2)
+    rows = (thready - 1, thready + 2)
+    cols = (threadx - 1, threadx + 2)
+    neighborhood = state_shot[rows[0]:rows[1]][cols[0]:cols[1]]
+    neighborhood[1][1] = 0
+    nh_sum = 0
+    for row in range(3):
+        for col in range(3):
+            nh_sum += neighborhood[row][col]
+    
+    neighborhoods_output[thready][threadx] = nh_sum
+
+@cuda.jit
+def get_next_shot(state_shot, neighborhoods, output_shot, rules_survive, rules_born):
+    """use neighborhood sum, state, and rules arrays to determine next state
+    for given cell/thread location"""
+    threadx, thready = cuda.grid(2)
+    current_state = state_shot[thready][threadx]
+    nh_sum = neighborhoods[thready][threadx]
+
+    if current_state: #if current state is on
+        for item in rules_survive:
+            if item == nh_sum:
+                output_shot[thready][threadx] = 1
+            else:
+                output_shot[thready][threadx] = 0
+    else: #current state is off
+        for item in rules_born:
+            if item == nh_sum:
+                output_shot[thready][threadx] = 1
+            else: 
+                output_shot[thready][threadx] = 0
 
