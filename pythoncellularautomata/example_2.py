5f0b4ad4f3c100d2ad02a36d4016882e584f11b1
Grant King <grantryan.king@gmail.com>
2020-03-18 23:48:47 -0700

    Update example_2.py
    Add new Grid arguments.  Add manual performance logging after simulation is finished.
    
    Move pygame display operations to be handled by Grid update.

--- pythoncellularautomata/example_2.py
+++ pythoncellularautomata/example_2.py
@@ -2,9 +2,10 @@
 import pygame
 from pygame.locals import QUIT
 from models.ca_models import Grid, Capture, Control
+import os
 
-SCREEN_SIZE = [1600, 900]
-CELL_SIZE = 15
+SCREEN_SIZE = [1920, 1080]
+CELL_SIZE = 10
 BACKGROUND_COLOR = [0, 0, 0]
 
 def main_loop():
@@ -13,19 +14,19 @@ def main_loop():
     clock = pygame.time.Clock()
     main_window = pygame.display.set_mode(SCREEN_SIZE)
     
-    grid = Grid(CELL_SIZE, 'maze', aging=1)
+    grid = Grid(CELL_SIZE, 'coral', aging=1, processing_mode=2)
     control = Control(grid)
     
-    control.capture.load_image("D:/chaos/extra/smoothie.png")
-    control.step_clock.set_timer(QUIT, 650) #auto off after n steps
-    control.step_clock.set_timer(control.PERFSTATSEVENT, 100) #
+    control.capture.load_image("D:/chaos/extra/tgcc.jpg")
+    control.step_clock.set_timer(QUIT, 1950) #auto off after n steps
+    control.step_clock.set_timer(control.PERFSTATSEVENT, 100) #log performance every 100 steps
     
     while control.running:
+        #clock.tick(22)
         grid.update()
-        pygame.display.set_caption(f'{grid.rule_set.name} step {grid.rule_set.run_ticks}')
-        pygame.display.flip()
     
     pygame.quit()
+    control.write_performance()
 
 if __name__ == '__main__':
     main_loop()
