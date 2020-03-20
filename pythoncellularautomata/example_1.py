3d9fd727c2782ee4f95df141b4664db54c9bd993
Grant King <grantryan.king@gmail.com>
2020-03-19 22:29:52 -0700

    Update example_1.py
    Update this first example to be simple. Move example that saves to disk to new file as example_3.py.

--- pythoncellularautomata/example_1.py
+++ pythoncellularautomata/example_1.py
@@ -2,8 +2,8 @@ import pygame
 from pygame.locals import QUIT
 from models.ca_models import Grid, Capture, Control
 
-SCREEN_SIZE = [1600, 900]
-CELL_SIZE = 10
+SCREEN_SIZE = [1920, 1080]
+CELL_SIZE = 3
 BACKGROUND_COLOR = [0, 0, 0]
 
 def main_loop():
@@ -12,23 +12,22 @@ def main_loop():
     clock = pygame.time.Clock()
     main_window = pygame.display.set_mode(SCREEN_SIZE)
     
-    grid = Grid(CELL_SIZE, 'life34', aging=1)
+    grid = Grid(CELL_SIZE, 'coagulations', show_colors=True, aging=True)
     control = Control(grid)
-    
+    print(control.CONTROLS)
+
     #grid.random_seed()
-    control.capture.load_image("D:/chaos/extra/smoothie.png")
-    control.step_clock.set_timer(QUIT, 650) #auto off after n steps
-    control.step_clock.set_timer(control.STATESHOTEVENT, 1) #capture state every n steps
+    control.capture.load_image("D:/chaos/extra/fish.jpg")
+    control.step_clock.set_timer(QUIT, 1650) #auto off after n steps
     
+    #capture stateshot of seed
     control.capture.state_shot()
+
+    #frame update loop
     while control.running:
+        clock.tick(control.fps)
         grid.update()
-        control.capture.screen_shot()
-        pygame.display.set_caption(f'{grid.rule_set.name} step {grid.rule_set.run_ticks}')
-        pygame.display.flip()
-        control.capture.save_image()
-    #capture last state before quit        
-    control.capture.state_shot()
+
     pygame.quit()
 
 if __name__ == '__main__':
