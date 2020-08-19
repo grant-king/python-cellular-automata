55912c08b05067eb5492625298466e5aa924ab48
Grant King <grantryan.king@gmail.com>
2020-08-18 23:56:31 -0700

    Update session_models.py
    Add several SessionConfigurationManager methods to get it working with the other new models in this file.
    
    Add methods for making a new default or custom configurations and for starting a new session from the default or a custom configuration settings file.
    
    Renme seed_file_path variable names to seed_image_path in SessionConfiguration and CellularAutomatonSession models.

--- pythoncellularautomata/models/session_models.py
+++ pythoncellularautomata/models/session_models.py
@@ -1,42 +1,84 @@
+import configparser
+import os
 from models.ca_models import Grid, Capture, Control
 import pygame
 from pygame.locals import QUIT
 
 class SessionConfigurationManager:
     def __init__(self):
-        #todo if base config exists, load settings
-        #todo else make new base config
-        self.settings_dict = {}
+        self.input_dict = {}
+        self.config_dict = {}
 
-    def play_config(self):
-        pass
+    def input_config_name(self):
+        load_name = input('Type the session config name you want to load: ')
+        if os.path.isfile(f'{load_name}.conf'):
+            self.current_session = self.start_config(f'{load_name}.conf')
+
+    def start_config(self, config_filename):
+        """start session from config name and return session object"""        
+        self.load_config_settings(config_filename)
+        session = CellularAutomatonSession(self.get_session_config())
+        return session
+
+    def load_config_settings(self, config_filename):
+        config = configparser.ConfigParser()
+        config.read(config_filename)
+        self.config_dict = config['SESSION_SETUP']
+
+    def get_session_config(self):
+        """create and return new SessionConfiguration object from 
+        variables held in config_dict that can be used for making a new 
+        session"""
+        session_config = SessionConfiguration(
+            list(map(int, self.config_dict['screen_size'].split(','))),
+            int(self.config_dict['cell_size']),
+            self.config_dict['initial_ruleset'],
+            aging=int(self.config_dict['aging']),
+            processing_mode=int(self.config_dict['processing_mode']),
+            show_colors=int(self.config_dict['show_colors']),
+            seed_image_path=self.config_dict['seed_image_path']
+        )
+        return session_config
 
     def play_default(self):
         """Start default simulation from configuration file"""
-        #todo if default sim doesn't exist, create new one
+        #if default sim doesn't exist, create new one
         if os.path.isfile('default_simulation_settings.conf'):
-            #todo load settings
-            #todo play settings
-            pass
+            self.current_session = self.start_config('default_simulation_settings.conf')
         else:
             self.make_base()
+            self.current_session = self.start_config('default_simulation_settings.conf')
 
-    def make_new(self, name):
+    def make_new(self, new_name=None):
         """interactively create new named automaton config"""
-        if os.path.isfile(f'{name}.conf'):
-            print(f'There is already a configuration called {name}. Please choose a new name for this configuraiton.')
-            #todo call to whatever takes name input to recall this method
+        if new_name == None:
+            self.input_dict['name'] = input('Type a new name for this config: ')
         else:
-            print(f'You are about to create a new simulation configuration named {name}.conf')
+            self.input_dict['name'] = new_name
+
+        if os.path.isfile(f"{self.input_dict['name']}.conf"):
+            print(f"There is already a configuration called {self.input_dict['name']}. Please choose a new name for this configuraiton.")
+            self.make_new()
+        else:
+            print(f"You are about to create a new simulation configuration named {self.input_dict['name']}.conf")
             self.input_settings()
+            self.save_config()
             
     def input_settings(self):
-        self.settings_dict['screen_size'] = input("type maximum screen size as width, height: ")
-        self.settings_dict['cell_size'] = input("how many pixels square do you want the cells to be? ")
-        self.settings_dict['initial_ruleset'] = input("What ruleset do you want to start with? ")
-        self.settings_dict['show_colors'] = input("Do you want to show colors? 1 or 0: ")
-        self.settings_dict['aging'] = input('Do you want cell aging on? 1 or 0: ')
-        self.settings_dict['processing_mode'] = input('Do you have a CUDA-enabled GPU? 1 or 0: ')
+        self.input_dict['screen_size'] = input("type maximum screen size as width, height: ")
+        self.input_dict['cell_size'] = input("how many pixels square do you want the cells to be? ")
+        self.input_dict['initial_ruleset'] = input("What ruleset do you want to start with? ")
+        self.input_dict['aging'] = input('Do you want cell aging on? 1 or 0: ')
+        self.input_dict['processing_mode'] = input('Do you have a CUDA-enabled GPU? 1 or 0: ')
+        self.input_dict['show_colors'] = input("Do you want to show colors? 1 or 0: ")
+        self.input_dict['seed_image_path'] = input("Type the full path for the seed image: ")
+
+    def save_config(self):
+        """save configuration to file"""
+        new_config = configparser.ConfigParser()
+        new_config['SESSION_SETUP'] = self.input_dict
+        with open(f"{self.input_dict['name']}.conf", 'w+') as save_file:
+            new_config.write(save_file)
 
     def make_base(self):
         """create new base configuration"""
@@ -46,14 +88,14 @@ class SessionConfigurationManager:
 
 class SessionConfiguration:
     def __init__(self, screen_size, cell_size, ruleset_name, 
-    aging=1, processing_mode=2,  show_colors=1, seed_file_path=None):
+    aging=1, processing_mode=2,  show_colors=1, seed_image_path=None):
         self.screen_size = screen_size
         self.cell_size = cell_size
         self.ruleset_name = ruleset_name
         self.aging = aging
         self.processing_mode = processing_mode
         self.show_colors = show_colors
-        self.seed_file_path = seed_file_path
+        self.seed_image_path = seed_image_path
 
 
 class CellularAutomatonSession:
@@ -70,10 +112,10 @@ class CellularAutomatonSession:
             )
         self.control = Control(self.grid)
 
-        if session_config.seed_file_path is None:
+        if session_config.seed_image_path is None:
             self.grid.random_seed(0.05)
         else:
-            self.control.capture.load_image(session_config.seed_file_path)
+            self.control.capture.load_image(session_config.seed_image_path)
 
         self.run_sim()
         
