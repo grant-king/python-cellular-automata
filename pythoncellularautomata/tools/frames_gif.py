c77e279a6922a1c54a9a7a716036409d2a778383
Grant King <grantryan.king@gmail.com>
2020-08-31 22:54:05 -0700

    Fix frame sorting issue
    Add sort call to images list. Add sorting key function to numerically sort filenames based on the sets of digits in their name, rather than alphabetically.

--- pythoncellularautomata/tools/frames_gif.py
+++ pythoncellularautomata/tools/frames_gif.py
@@ -2,16 +2,23 @@ import imageio
 from pathlib import Path
 import os
 import sys
+import re
 
 def make_gif(frames_dir):
     image_path = Path(frames_dir)
     images = list(image_path.glob('image*.png'))
+    images.sort(key=get_filename_key)
     image_list = []
     for file_name in images:
         image_list.append(imageio.imread(file_name))
     
-    imageio.mimwrite(os.path.join(image_path ,'animation.gif'), image_list)
+    imageio.mimwrite(image_path.joinpath('animation.gif'), image_list)
 
+def get_filename_key(filename):
+    numbers = re.compile(r'(\d+)')
+    filename_nums = numbers.split(filename.name)[1::2]
+    image_position = list(map(int, filename_nums))
+    return image_position #return list of ints in filename for comparison
 
 if __name__ == "__main__":
     directory = sys.argv[1]
